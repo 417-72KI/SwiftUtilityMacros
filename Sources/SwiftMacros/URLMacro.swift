cb7fa026aa6d8837dc175c1445f368bf0fe5b685
417-72KI <417.72ki@gmail.com>
2023-01-22 02:27:47 +0900

    draft for `#url` macro

---
+++ Sources/SwiftMacros/URLMacro.swift
@@ -0,0 +1,48 @@
+import SwiftSyntax
+import SwiftSyntaxParser
+import _SwiftSyntaxMacros
+
+public enum URLMacro: ExpressionMacro {
+    public static func expansion(
+        of node: MacroExpansionExprSyntax,
+        in context: inout MacroExpansionContext
+    ) throws -> ExprSyntax {
+        // print(#line, node)
+        guard let (arguments, argument) = validate(node.argumentList) else {
+            // TODO: compile error
+            return ExprSyntax(node)
+        }
+        let argumentList = arguments.replacing(
+            childAt: 0,
+            with: argument
+                .withLabel(.identifier("string"))
+                .withColon(.identifier(": "))
+        )
+        return ExprSyntax(
+            ForcedValueExprSyntax(
+                expression: FunctionCallExprSyntax(
+                    calledExpression: IdentifierExprSyntax(identifier: TokenSyntax(.stringSegment("URL"), presence: .present)),
+                    leftParen: TokenSyntax(.leftParen, presence: .present),
+                    argumentList: argumentList,
+                    rightParen: TokenSyntax(.rightParen, presence: .present)
+                )
+            )
+        )
+    }
+}
+
+extension URLMacro {
+    static func validate(_ elementList: TupleExprElementListSyntax?) -> (TupleExprElementListSyntax, TupleExprElementSyntax)? {
+        guard let elementList,
+              let element = elementList.first else { return nil }
+        guard let expression = element.expression
+            .as(StringLiteralExprSyntax.self) else { return nil }
+        let segments = expression.segments
+        guard case let .stringSegment(segment) = segments.first else { return nil }
+        guard segment.contentLength.utf8Length > 0 else { return nil }
+        let content = segment.content.text
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+        guard !content.isEmpty else { return nil }
+        return (elementList, element)
+    }
+}
---
+++ Tests/SwiftMacrosTests/SwiftMacrosTests.swift
@@ -0,0 +1,54 @@
+import XCTest
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftMacros
+@_spi(Testing) import _SwiftSyntaxMacros
+// import _SwiftSyntaxTestSupport
+
+final class SwiftMacrosTests: XCTestCase {
+    func testUrl() throws {
+        let sf: SourceFileSyntax = ##"""
+        let a = #url("https://apple.com")
+        let b = #url("""
+        https://google.com
+        """)
+        let c = #url(#"https://github.com"#)
+        let d = #url(#"""
+            https://amazon.com
+            """#)
+        let e = #url("")
+        let f = #url("""
+        """)
+        let g = #url("""
+
+        """)
+        """##
+
+        var context = MacroExpansionContext(
+            moduleName: "MyModule", fileName: "test.swift"
+        )
+        let transformedSF = sf.expand(
+            macros: ["url": URLMacro.self],
+            in: &context
+        )
+        XCTAssertEqual(
+            transformedSF.description,
+            ##"""
+            let a = URL(string: "https://apple.com")!
+            let b = URL(string: """
+            https://google.com
+            """)!
+            let c = URL(string: #"https://github.com"#)!
+            let d = URL(string: #"""
+                https://amazon.com
+                """#)!
+            let e = #url("")
+            let f = #url("""
+            """)
+            let g = #url("""
+
+            """)
+            """##
+        )
+    }
+}
